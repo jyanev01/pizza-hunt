5f366bf7c862718fc5e68e019dc7de63e1b4cdee
jyanev01 <jordanyanev@gmail.com>
2022-01-08 18:50:15 -0500

    updated"

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,18 @@
 const { Schema, model } = require('mongoose');
 
-const commentSchema = new Schema ({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Comment = model('Comment', commentSchema);
+const Comment = model('Comment', CommentSchema);
 
-module.exports =  Comment;
+module.exports = Comment;
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,37 +1,37 @@
 const { Schema, model } = require('mongoose');
-const Pizza = model('Pizza', PizzaSchema);
 const dateFormat = require('../utils/dateFormat');
 
-
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    size: {
+      type: String,
+      default: 'Large'
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
   },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal)
-  },
-  size: {
-    type: String,
-    default: 'Large'
-  },
-  toppings: [],
-  comments: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Comment'
-    }
-  ]
-},
   {
     toJSON: {
       virtuals: true,
-      getters: true,
+      getters: true
     },
+    // prevents virtuals from creating duplicate of _id as `id`
     id: false
   }
 );
@@ -41,5 +41,6 @@ PizzaSchema.virtual('commentCount').get(function() {
   return this.comments.length;
 });
 
+const Pizza = model('Pizza', PizzaSchema);
 
 module.exports = Pizza;
